842781315104c45daa3c5d99ad7ebc0af08a8782
Oliver Wagner <wagnerof@mail.uc.edu>
2018-02-14 14:08:22 -0500

    Updated the NFCRead file

--- ILSA/static/ILSA/NFCRead.js
+++ ILSA/static/ILSA/NFCRead.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
-    var poll = true;
-    while (poll) {
-        $.ajax({
-            type: "POST",
-            url: "http://192.168.1.1:8000/ILSA/NFC",
-            dataType: "text",
-            async: false,
-            success: function(data) {
-                if (data === "No Swipe") {
-                    window.location.href = 'http://192.168.1.1:8000/ILSA/';
-                }
-                else if (data === "Error") {
-                    window.location.href = 'http://192.168.1.1:8000/ILSA/';
-                } else {
-                    poll = false;
-                    $.ajax({
-                        type: "POST",
-                        url: "http://192.168.1.1:8000/ILSA/swipe/" + data,
-                        async: false,
-                        success: function (swipe_response) {
-                            window.location.href = swipe_response;
-                        }
-                    });
-                }
-            },
-            error: function() {
-               // alert('Error');
+    $.ajax({
+        type: "GET",
+        url: "http://192.168.1.1:8000/ILSA/NFC",
+        dataType: "text",
+        async: false,
+        success: function(data) {
+            if (data === "No Swipe") {
+                location.reload(true);
             }
-        });
-    }
+            else if (data === "Error") {
+                location.reload(true);
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: "http://192.168.1.1:8000/ILSA/swipe/" + data,
+                    async: false,
+                    success: function (swipe_response) {
+                        window.location.href = swipe_response;
+                    }
+                });
+            }
+        },
+        error: function() {
+            // alert('Error');
+        }
+    });
 });
