54d6469d5b93e5bc107d3658ba16d1530e895a9e
Oliver Wagner <wagnerof@mail.uc.edu>
2018-02-14 17:05:45 -0500

    Finished ajax calls for NFCRead

--- ILSA/static/ILSA/NFCRead.js
+++ ILSA/static/ILSA/NFCRead.js
@@ -16,7 +16,15 @@ $(document).ready(function () {
                     url: "http://192.168.1.1:8000/ILSA/swipe/" + data + "/",
                     async: false,
                     success: function (swipe_response) {
-                        window.location.href = swipe_response;
+                        var value = swipe_response.response;
+                        var url = "http://192.168.1.1:8000/";
+                        if (value === "admin") {
+                            window.location.href = url + value + "/";
+                        } else if (value === "lockers") {
+                            window.location.href = url + "ILSA/" + value + "/";
+                        } else if (value === "success") {
+                            window.location.href = url + "ILSA/lockers/" + value + "/";
+                        }
                     }
                 });
             }
--- ILSA/urls.py
+++ ILSA/urls.py
@@ -6,9 +6,9 @@ urlpatterns =[
     path('', views.HomepageView, name='home page'),
     path('lockers/', views.LockersView, name='lockers'),
     path('swipe/<str:card_uid>/', views.swipe, name='swipe'),
-    path('lockers/check_in', views.check_in, name='check in'),
+    path('lockers/check_in/', views.check_in, name='check in'),
     path('lockers/arduino/<str:mac_address>/<int:battery_level>/', views.arduino, name='arduino'),
-    path('lockers/success', views.success, name='success'),
+    path('lockers/success/', views.success, name='success'),
     path('NFC', views.NFC, name='NFC'),
     path('swipetest', views.swipetest, name='swipe test'),
 ]
